79a77ca5716c58f7cfeb0821e4b5b16df9fa9786
Raimond Fowler <raifowler@gmail.com>
2018-08-02 17:42:08 -0700

    Merge context.js to master branch for state handling

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,34 +1,8 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "jdoe@gmail.com",
-          phone: "555-555-5555"
-        },
-        {
-          id: 2,
-          name: "Karen Williams",
-          email: "karen@gmail.com",
-          phone: "333-333-3333"
-        },
-        {
-          id: 3,
-          name: "Henry Johnson",
-          email: "henry@gmail.com",
-          phone: "111-111-1111"
-        }
-      ]
-    };
-  }
-
   deleteContact = id => {
     const { contacts } = this.state;
     const newContacts = contacts.filter(contact => contact.id !== id);
@@ -37,18 +11,24 @@ class Contacts extends Component {
   };
 
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
